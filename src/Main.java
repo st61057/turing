f91b668f45c2142824f3eef3f91d021c9b5627ac
st61057 <st61057@upce.cz>
2024-03-27 23:10:01 +0100

    Dodelani semestralky

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         TuringMachine turingMachine = new TuringMachine();
-        turingMachine.run("111B11", "src//addRules.txt");
+//        turingMachine.run("111B11", "src//addRules.txt"); // +
+//        turingMachine.run("11111B111B", "src//subtractionRules.txt");// -
+//        turingMachine.run("aaabbcccB", "src//a+b+c+.txt"); // a+b+c+
+//        turingMachine.run("aacccB", "src//axbxcx.txt"); // a*b*c*
     }
 }
